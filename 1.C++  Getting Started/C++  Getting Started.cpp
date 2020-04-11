4144e29156f29b3456b38ca46d28aa853485f08b
SonicMetallica <reneschicht@kabelmail.de>
2020-04-11 20:56:20 +0200

    lol

---
+++ 1.C++  Getting Started/C++  Getting Started.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello World! i am on my way.\n" << endl;
+    cout << "Hello again!" << endl;
+    cout << "Hello again!" << endl;
+    
+    return 0;
+}
+
